f41e8697002c2c9e8b53cdf9c584ec12079f790a
Denis Truong <truongd@ku.edu>
2022-04-25 15:27:50 -0500

    added dontenv config changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+require('dotenv').config();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
+app.listen(process.env.LOCAL_LISTENING_PORT, () => {
+  console.log(`Listening on port ${process.env.LOCAL_LISTENING_PORT}`);
 });
